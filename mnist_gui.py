3a0229cd192dab117faeae20dc6dcca0c2132ce7
tansunogon <matsubara.norhide.24r@st.kyoto-u.ac.jp>
2017-11-09 16:54:31 +0900

    不要な関数を削除 (Open, mabeSave など)

--- mnist_gui.py
+++ mnist_gui.py
@@ -136,28 +136,6 @@ class ScribbleArea(QWidget):
     def showImage(self):
         self.showacc()
 
-    def openImage(self, fileName):
-        loadedImage = QImage()
-        if not loadedImage.load(fileName):
-            return False
-
-        newSize = loadedImage.size().expandedTo(self.size())
-        self.resizeImage(loadedImage, newSize)
-        self.image = loadedImage
-        self.modified = False
-        self.update()
-        return True
-
-    def saveImage(self, fileName, fileFormat):
-        visibleImage = self.image
-        self.resizeImage(visibleImage, self.size())
-
-        if visibleImage.save(fileName, fileFormat):
-            self.modified = False
-            return True
-        else:
-            return False
-
     def setPenColor(self, newColor):
         self.myPenColor = newColor
 
@@ -219,20 +197,6 @@ class ScribbleArea(QWidget):
         painter.drawImage(QPoint(0, 0), image)
         self.image = newImage
 
-    def print_(self):
-        printer = QPrinter(QPrinter.HighResolution)
-
-        printDialog = QPrintDialog(printer, self)
-        if printDialog.exec_() == QPrintDialog.Accepted:
-            painter = QPainter(printer)
-            rect = painter.viewport()
-            size = self.image.size()
-            size.scale(rect.size(), Qt.KeepAspectRatio)
-            painter.setViewport(rect.x(), rect.y(), size.width(), size.height())
-            painter.setWindow(self.image.rect())
-            painter.drawImage(0, 0, self.image)
-            painter.end()
-
     def isModified(self):
         return self.modified
 
@@ -255,11 +219,11 @@ class MainWindow(QMainWindow):
         self.createActions()
         self.createMenus()
 
-        self.setWindowTitle("Scribble")
+        self.setWindowTitle("MNIST GUI")
         self.resize(500, 500)
 
     def closeEvent(self, event):
-        if self.maybeSave():
+        if self.exitWarn():
             event.accept()
         else:
             event.ignore()
@@ -267,65 +231,24 @@ class MainWindow(QMainWindow):
     def showImage(self):
         self.scribbleArea.showImage()
 
-    def open(self):
-        if self.maybeSave():
-            fileName, _ = QFileDialog.getOpenFileName(self, "Open File",
-                                                      QDir.currentPath())
-            if fileName:
-                self.scribbleArea.openImage(fileName)
-
-    def save(self):
-        action = self.sender()
-        fileFormat = action.data()
-        self.saveFile(fileFormat)
-
     def penColor(self):
         newColor = QColorDialog.getColor(self.scribbleArea.penColor())
         if newColor.isValid():
             self.scribbleArea.setPenColor(newColor)
 
     def penWidth(self):
-        newWidth, ok = QInputDialog.getInt(self, "Scribble",
+        newWidth, ok = QInputDialog.getInt(self, "MNIST GUI",
                                            "Select pen width:", self.scribbleArea.penWidth(), 1, 50, 1)
         if ok:
             self.scribbleArea.setPenWidth(newWidth)
 
     def about(self):
-        QMessageBox.about(self, "About Scribble",
-                          "<p>The <b>Scribble</b> example shows how to use "
-                          "QMainWindow as the base widget for an application, and how "
-                          "to reimplement some of QWidget's event handlers to receive "
-                          "the events generated for the application's widgets:</p>"
-                          "<p> We reimplement the mouse event handlers to facilitate "
-                          "drawing, the paint event handler to update the application "
-                          "and the resize event handler to optimize the application's "
-                          "appearance. In addition we reimplement the close event "
-                          "handler to intercept the close events before terminating "
-                          "the application.</p>"
-                          "<p> The example also demonstrates how to use QPainter to "
-                          "draw an image in real time, as well as to repaint "
-                          "widgets.</p>")
+        QMessageBox.about(self, "About MNIST GUI",
+                          "<p>The <b>MNIST GUI</b> provides hand-drawing tests.")
 
     def createActions(self):
         self.showAct = QAction("&showImage", self,
                                triggered=self.showImage)
-        self.openAct = QAction("&Open...", self, shortcut="Ctrl+O",
-                               triggered=self.open)
-
-        for format in QImageWriter.supportedImageFormats():
-            aaaaa = bytes(format)
-            #format = str(format)
-            format = aaaaa.decode('utf-8')
-            print("format: %s" % format)
-
-            text = format.upper() + "..."
-
-            action = QAction(text, self, triggered=self.save)
-            action.setData(format)
-            self.saveAsActs.append(action)
-
-        self.printAct = QAction("&Print...", self,
-                                triggered=self.scribbleArea.print_)
 
         self.exitAct = QAction("E&xit", self, shortcut="Ctrl+Q",
                                triggered=self.close)
@@ -345,15 +268,8 @@ class MainWindow(QMainWindow):
                                   triggered=QApplication.instance().aboutQt)
 
     def createMenus(self):
-        self.saveAsMenu = QMenu("&Save As", self)
-        for action in self.saveAsActs:
-            self.saveAsMenu.addAction(action)
-
         fileMenu = QMenu("&File", self)
         fileMenu.addAction(self.showAct)
-        fileMenu.addAction(self.openAct)
-        fileMenu.addMenu(self.saveAsMenu)
-        fileMenu.addAction(self.printAct)
         fileMenu.addSeparator()
         fileMenu.addAction(self.exitAct)
 
@@ -371,19 +287,14 @@ class MainWindow(QMainWindow):
         self.menuBar().addMenu(optionMenu)
         self.menuBar().addMenu(helpMenu)
 
-    def maybeSave(self):
-        if self.scribbleArea.isModified():
-            ret = QMessageBox.warning(self, "Scribble",
-                                      "The image has been modified.\n"
-                                      "Do you want to save your changes?",
-                                      QMessageBox.Save | QMessageBox.Discard |
-                                      QMessageBox.Cancel)
-            if ret == QMessageBox.Save:
-                return self.saveFile('png')
-            elif ret == QMessageBox.Cancel:
-                return False
-
-        return True
+    def exitWarn(self):
+        ret = QMessageBox.warning(self, "MNIST GUID",
+                                  "Are you sure to exit?",
+                                  QMessageBox.Ok | QMessageBox.Cancel)
+        if ret == QMessageBox.Ok:
+            return True
+        else:
+            return False
 
     def saveFile(self, fileFormat):
 
@@ -394,9 +305,11 @@ class MainWindow(QMainWindow):
                                                   "%s Files (*.%s);;All Files (*)" % (fileFormat.upper(), fileFormat))
 
         if fileName:
-            return self.scribbleArea.saveImage(fileName, fileFormat)
+            print("dummy saveFile")
+            return True;
 
         return False
+
 
 if __name__ == '__main__':
 
